eb94682c5362f3835b58974d0a48c4ec85a5b060
Srushti Gol <105737505+Srushti-Gol@users.noreply.github.com>
2024-02-14 21:04:33 +0530

    working Agribot

--- ml/Flask/chat.py
+++ ml/Flask/chat.py
@@ -1,32 +1,33 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# from openai import OpenAI
+from dotenv import load_dotenv
+from openai import OpenAI
 import os
 
 app = Flask(__name__)
 
 CORS(app)  
-# key = os.getenv('OPENAI_API_KEY')
-# os.environ['OPENAI_API_KEY'] = ""
-# client = OpenAI(api_key = os.environ['OPENAI_API_KEY'])
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key = OPENAI_API_KEY)
 
-# def generate_response(user_message):
+def generate_response(user_message):
     
-#         response = client.chat.completions.create(
-#             model = "text-davinci-002",
-#             messages = [
-#                 {"role" : "user" , "content" : user_message}
-#             ] 
-#         )
-#         return response.choices[0].message
+        response = client.chat.completions.create(
+            model = "gpt-3.5-turbo",
+            messages = [
+                {"role" : "user" , "content" : user_message}
+            ] 
+        )
+        return response.choices[0].message.content
     
 @app.route('/chat', methods=['POST'])
 def chat():
         data = request.get_json()
         user_message = data.get('text')
-        bot_message = "Bot Reply"
-        # bot_message = generate_response(user_message)
-        # print("Bot's response:", bot_message)  
+        # bot_message = "Bot Reply"
+        bot_message = generate_response(user_message)
+        print("Bot's response:", bot_message)  
         return jsonify({'message': bot_message})
   
 if __name__ == '__main__':
